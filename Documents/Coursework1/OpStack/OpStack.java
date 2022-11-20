c939661c32a1ebde65596fd87679a58ff4c7573f
Aryaman <zjac180@live.rhul.ac.uk>
2022-11-20 19:37:50 +0000

    Name:Aryaman Rawat Date:20/11/2022
    A new feature branch is created to implement OpStack which is a facade
    class and will function similarly to NumStack but with symbols.
    
    So far OpStack.java and TestOpStack files have been added the test file
    only contains a single test case following TDD approach it has passed.
    
    Javadoc and comments will be added each time I make changes and add new
    test cases.

---
+++ Documents/Coursework1/OpStack/OpStack.java
@@ -0,0 +1,11 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * The NumStack class contains a numStack.
+ *
+ * @author Aryaman Rawat
+ *
+ */
+public class OpStack {
+
+}
---
+++ Documents/Coursework1/OpStack/TestOpStack.java
@@ -0,0 +1,17 @@
+package uk.ac.rhul.cs2800;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestOpStack {
+  OpStack stack;
+
+  @Test
+  @BeforeEach
+  void testClass() {
+    // creating a new stack to pass test using TDD cycle
+    stack = new OpStack();
+
+
+  }
+}
