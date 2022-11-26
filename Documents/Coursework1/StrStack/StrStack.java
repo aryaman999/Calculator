42a3f8dccbeabf0a2ceb3c2ccb6816cd3c5d08bf
Aryaman <zjac180@live.rhul.ac.uk>
2022-11-26 14:24:51 +0000

    Name:Aryaman Rawat Date:26/11/2022 This feature branch is needed to evaluate the StrStack class and implement all its responsibilities following the red green commit TDD cycle.
    First test case simply implements a public class in StrStack.java.

---
+++ Documents/Coursework1/StrStack/StrStack.java
@@ -0,0 +1,11 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * The StrStack class contains strStack.
+ *
+ * @author zjac180
+ *
+ */
+public class StrStack {
+
+}
---
+++ Documents/Coursework1/StrStack/TestStrStack.java
@@ -0,0 +1,18 @@
+package uk.ac.rhul.cs2800;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestStrStack {
+  StrStack stack;
+
+  @Test
+  @BeforeEach
+  void testClass() {
+
+    stack = new StrStack();
+
+
+  }
+
+}
