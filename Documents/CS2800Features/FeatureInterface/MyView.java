d2f64b49bc96db5415fa25eaab8efd6591d445f3
Aryaman <zjac180@live.rhul.ac.uk>
2022-11-30 19:02:25 +0000

    Name:Aryaman Rawat    Date:30/11/2022
    The interface now has a function added to it when the button is pressed,
    the label sign changes from label to pressed.
    
    This was done by adding a new action event in MyView.java.

--- Documents/CS2800Features/FeatureInterface/MyView.java
+++ Documents/CS2800Features/FeatureInterface/MyView.java
@@ -1,5 +1,22 @@
 package uk.ac.rhul.cs2800;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MyView {
+
+  @FXML
+  private Label label;
+
+  @FXML
+  private Button mainButton;
+
+  @FXML
+  void isPressed(ActionEvent event) {
+    label.setText("pressed");
+
+  }
 
 }
