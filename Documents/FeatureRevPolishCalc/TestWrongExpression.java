6a6d78873eb4b81950185245c0f0ed894a8769d8
Aryaman <zjac180@live.rhul.ac.uk>
2022-12-03 17:07:30 +0000

    Name:Aryaman Rawat     Date:03/12/2022
    Created a custom expection which checks if expression is invalid.
    Added a class which constructs a message using super key.Java doc and
    checkstyle has all been updated and meets the requirements.This
    exception is complete and will be implmented in RevPolishCalc class.

---
+++ Documents/FeatureRevPolishCalc/TestWrongExpression.java
@@ -0,0 +1,17 @@
+package uk.ac.rhul.cs2800;
+
+
+import org.junit.jupiter.api.Test;
+
+class TestWrongExpression {
+  @SuppressWarnings("unused")
+  @Test
+  void testClass() {
+
+    WrongExpression exception = new WrongExpression();
+    WrongExpression exceptionMessage = new WrongExpression("Test to check for exception");
+
+
+  }
+
+}
