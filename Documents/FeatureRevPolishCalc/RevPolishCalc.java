06f16e85a39b2cd36b6daac74d8a30b762e7b007
Aryaman <zjac180@live.rhul.ac.uk>
2022-12-03 15:30:08 +0000

    Name:Aryaman Rawat      Date:03/12/2022
    The feature RevPolishCalc will evaluate reverse polish string
    expressions.A new test case added follwing the red, green,refactor cycle which simply creates a class for RevPolishCalc.

---
+++ Documents/FeatureRevPolishCalc/RevPolishCalc.java
@@ -0,0 +1,11 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * The RevPolishCalc class evaluates string expressions.
+ *
+ * @author Aryaman Rawat
+ *
+ */
+public class RevPolishCalc {
+
+}
---
+++ Documents/FeatureRevPolishCalc/TestRevPolishCalc.java
@@ -0,0 +1,19 @@
+package uk.ac.rhul.cs2800;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class TestRevPolishCalc {
+  RevPolishCalc calculator;
+
+  @Test
+  @BeforeEach
+  void testClass() {
+    calculator = new RevPolishCalc(); // Creates RevPolishCalc class.
+
+  }
+
+
+
+}
