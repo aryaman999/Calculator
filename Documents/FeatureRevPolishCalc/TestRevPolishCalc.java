18aeefae510297a246e9e61babdc74e2be90ab76
Aryaman <zjac180@live.rhul.ac.uk>
2022-12-10 13:09:19 +0000

    Name:Aryaman Rawat      Date:10/12/2022
    Added a new test case which checks for multiple evaluation the
    expression given.After passing the test by faking,I implemented the
    calculate method which takes the left and right side of the string
    expression and calculates the answer by checking for the given operator
    in that expression.

--- Documents/FeatureRevPolishCalc/TestRevPolishCalc.java
+++ Documents/FeatureRevPolishCalc/TestRevPolishCalc.java
@@ -20,3 +20,21 @@ class TestRevPolishCalc {
   void testSingle() throws WrongExpression {
     assertEquals(calculator.evaluate("2 4 +"), 6f,
         "test should give right answer as expression is valid");
+
+
+  }
+
+  @Test
+  void testMultiple() throws WrongExpression {
+    assertEquals(calculator.evaluate("2 4 +"), 6f,
+        "test should give right answer as expression is valid");
+    assertEquals(calculator.evaluate("6 4 -"), 2f,
+        "test should give right answer as expression is valid");
+    assertEquals(calculator.evaluate("2 4 *"), 8f,
+        "test should give right answer as expression is valid");
+    assertEquals(calculator.evaluate("6 2 /"), 3f,
+        "test should give right answer as expression is valid");
+
+
+  }
+}
